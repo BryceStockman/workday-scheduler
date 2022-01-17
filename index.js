666ddcd34094ecff4774533269b5b035002f86b8
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-01-17 15:41:08 -0700

    content is currently being added

--- index.js
+++ index.js
@@ -1,2 +1,46 @@
-var currentDay = document.querySelector('#currentDay');
+const currentDay = document.querySelector('#currentDay');
+const currentDate = moment().format('dddd, MMMM Do yyyy');
 currentDay.innerHTML = currentDate;
+
+const timeBlock = $('.time-block').text();
+
+// for (var i = 0; i < timeBlock.length; i++) {
+//   console.log(timeBlock[i]);
+// }
+
+let events = {};
+
+const createEvent = function (eventText) {
+  const description = $('.description');
+
+  const eventP = $('<p>')
+    .addClass('event-text')
+    .addClass('m-1')
+    .text(eventText);
+
+  description.append(eventP);
+};
+
+$('.description').click(function (e) {
+  e.preventDefault();
+  addEvent = e.target;
+
+  let text = $(this).text().trim();
+  let textInput = $('<textarea>').addClass('event-input').val(text);
+  $(this).html(textInput);
+  textInput.trigger('focus');
+});
+
+$('.description').on('blur', 'textarea', function () {
+  let text = $(this).val().trim();
+
+  events = text;
+  saveEvents();
+  console.log(events);
+});
+
+const saveEvents = function () {
+  localStorage.setItem('events', JSON.stringify(events));
+};
+
+$('.description').trigger('blur');
