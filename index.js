53d1b01a70bd4ec970bea93f9605677daa319fa0
BryceStockman <65779581+BryceStockman@users.noreply.github.com>
2022-01-26 18:48:23 -0700

    finished

--- index.js
+++ index.js
@@ -1,78 +1,56 @@
-const currentDay = document.querySelector('#currentDay');
-const currentDate = moment().format('dddd, MMMM Do yyyy');
-currentDay.innerHTML = currentDate;
+$(document).ready(function () {
+  const currentDay = document.querySelector('#currentDay');
+  const currentDate = moment().format('dddd, MMMM Do yyyy');
+  currentDay.innerHTML = currentDate;
+  const currentTime = parseInt(moment().format('HH'));
 
-const currentTime = moment().format('HH:mm:ss a');
-console.log(currentTime);
+  const appointments = [];
 
-// QUESTION: NOT SURE IF THIS IS HOW I NEED TO DO THIS?
-const firstTimeBlock = $('#9am').text();
-console.log(firstTimeBlock);
-const secondTimeBlock = $('#10am').text();
-const thirdTimeBlock = $('#11am').text();
-const fourthTimeBlock = $('#12pm').text();
-const fifthTimeBlock = $('#1pm').text();
-const sixthTimeBlock = $('#2pm').text();
-const seventhTimeBlock = $('#3pm').text();
-const eighthTimeBlock = $('#4pm').text();
-const ninethTimeBlock = $('#5pm').text();
+  const auditAppointments = function () {
+    $('textarea').each(function () {
+      var hour = parseInt($(this).attr('id'));
 
-// QUESTION: should this be an array or object?
-let events = {};
+      if (hour < currentTime) {
+        $(this).addClass('past');
+      } else if (hour === currentTime) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  };
 
-// QUESTION: How do I target the text and loop through each instance?
-const timeBlock = $('.time-block').html();
+  const loadAppointments = function () {
+    $('#9').val(localStorage.getItem('9'));
+    $('#10').val(localStorage.getItem('10'));
+    $('#11').val(localStorage.getItem('11'));
+    $('#12').val(localStorage.getItem('12'));
+    $('#1').val(localStorage.getItem('1'));
+    $('#2').val(localStorage.getItem('2'));
+    $('#3').val(localStorage.getItem('3'));
+    $('#4').val(localStorage.getItem('4'));
+    $('#5').val(localStorage.getItem('5'));
+  };
 
-const auditEventTime = function () {
-  // for (var i = 0; i < timeBlock.length; i++) {
-  //   events = timeBlock[i];
-  //   console.log(events);
-  // }
+  $('.saveBtn').click(function () {
+    let appointmentText = $(this).siblings('.description').val();
 
-  if (timeBlock <= currentTime) {
-    $(timeBlock).addClass('past');
-  } else if (timeBlock == currentTime) {
-    $(timeBlock).addClass('preset');
-  } else if (timeBlock >= currentTime) {
-    $(timeBlock).addClass('future');
-  }
-};
+    appointmentId = $(this).siblings('.description').attr('id');
 
-auditEventTime();
+    let appointment = {
+      hour: appointmentId,
+      text: appointmentText,
+    };
 
-const createEvent = function (eventText) {
-  const description = $('.description');
+    appointments.push(appointment);
+    console.log(appointmentText);
 
-  const eventP = $('<p>')
-    .addClass('event-text')
-    .addClass('m-1')
-    .text(eventText);
+    localStorage.setItem(appointmentId, appointmentText);
+  });
 
-  description.append(eventP);
-};
-
-$('.description').click(function (e) {
-  e.preventDefault();
-  addEvent = e.target;
-
-  let text = $(this).text().trim();
-  let textInput = $('<textarea>').addClass('event-input').val(text);
-  $(this).html(textInput);
-  textInput.trigger('focus');
+  loadAppointments();
+  auditAppointments();
 });
-
-$('.description').on('blur', 'textarea', function () {
-  let text = $(this).val().trim();
-
-  // QUESTION: When this is in an array, it overrides the previous instance
-  // QUESTION: When I put this in an object it adds every instance and doesn't replace the old instance
-  let event = text;
-  events.push(event);
-  saveEvents();
-});
-
-const saveEvents = function () {
-  localStorage.setItem('events', JSON.stringify(events));
-};
-
-$('.description').trigger('blur');
